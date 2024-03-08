588753bb90b7d67274b06b441f17ed6d1b018ebb
Zoran Zaric <zz@zoranzaric.de>
2024-03-08 23:04:14 +0100

    Add git-silent-add

--- home-common.nix
+++ home-common.nix
@@ -8,6 +8,22 @@
 
       du-dust
     ];
+
+    file = {
+      "${config.home.homeDirectory}/.local/bin/git-silent-add" = {
+        enable = true;
+        executable = true;
+        text =
+          ''
+            #!/bin/bash
+
+            set -e
+
+            git add --force --intent-to-add $@
+            git update-index --assume-unchanged $@
+          '';
+      };
+    };
   };
 
   programs.home-manager.enable = true;
--- home/zoranzaric/default.nix
+++ home/zoranzaric/default.nix
@@ -3,21 +3,5 @@
   home = {
     username = "zoranzaric";
     homeDirectory = lib.mkForce "/Users/zoranzaric";
-
-    file = {
-      "${config.home.homeDirectory}/.local/bin/git-silent-add" = {
-        enable = true;
-        executable = true;
-        text =
-          ''
-            #!/bin/bash
-
-            set -e
-
-            git add --force --intent-to-add $@
-            git update-index --assume-unchanged $@
-          '';
-      };
-    };
   };
 }
