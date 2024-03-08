2c7259e57844a7469995c7736de6f665ee4df5e9
Zoran Zaric <zz@zoranzaric.de>
2024-03-08 23:01:26 +0100

    6   2024-03-08 06:26:35   (current)

--- home/zoranzaric/default.nix
+++ home/zoranzaric/default.nix
@@ -1,7 +1,23 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   home = {
     username = "zoranzaric";
     homeDirectory = lib.mkForce "/Users/zoranzaric";
+
+    file = {
+      "${config.home.homeDirectory}/.local/bin/git-silent-add" = {
+        enable = true;
+        executable = true;
+        text =
+          ''
+            #!/bin/bash
+
+            set -e
+
+            git add --force --intent-to-add $@
+            git update-index --assume-unchanged $@
+          '';
+      };
+    };
   };
 }
